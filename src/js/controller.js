0deb36dd39cb64ecf12eac9bbe6d8a5d4bfe3204
PhilipZhangs <zhangsphilip@gmail.com>
2021-09-13 21:28:02 -0700

    adding event handlers for load and hashchange

--- src/js/controller.js
+++ src/js/controller.js
@@ -31,12 +31,14 @@ const renderSpinner = function(parentEl) {
 
 const showRecipe = async function() {
   try {
+    const id = window.location.hash.slice(1);
+
+    if(!id) return;
 
     // 1) Loading recipe
     renderSpinner(recipeContainer);
     const res = await fetch(
-      // 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcc5b'
-      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
     );
     const data = await res.json();
 
@@ -147,6 +149,6 @@ const showRecipe = async function() {
   } catch(err) {
     alert(err)
   }
-}
+};
 
-showRecipe();
+['hashchange', 'load'].forEach(ev => window.addEventListener(ev, showRecipe))
