0254d7a8b5655db8faf7eb2a010158b371393e46
PhilipZhangs <zhangsphilip@gmail.com>
2021-09-21 00:33:43 -0700

    created error and success messages for web page

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,9 +1,12 @@
 import icons from 'url:../../img/icons.svg'; // Parcel 2
 import Fraction from 'fractional';
+// import { hasOwnMetadata } from 'core-js/fn/reflect';
 
 class RecipeView {
     #parentElement = document.querySelector('.recipe');
     #data;
+    #errorMessage = 'Could not find recipe. Please try another.';
+    #message = '';
 
     render(data) {
         this.#data = data;
@@ -16,7 +19,7 @@ class RecipeView {
         this.#parentElement.innerHTML = '';
     }
 
-    renderSpinner = function() {
+    renderSpinner() {
     const markup = `
         <div class="spinner">
             <svg>
@@ -27,6 +30,36 @@ class RecipeView {
       this.#parentElement.innerHTML = '';
       this.#parentElement.insertAdjacentHTML('afterbegin', markup);
   };
+
+  renderError(message = this.#errorMessage) {
+      const markup = `
+      <div class="error">
+            <div>
+              <svg>
+                <use href="${icons}#icon-alert-triangle"></use>
+              </svg>
+            </div>
+            <p>${message}</p>
+          </div>
+          `;
+          this.#clear();
+          this.#parentElement.insertAdjacentHTML('afterbegin', markup);
+  }
+
+  renderMessage(message = this.#message) {
+    const markup = `
+    <div class="message">
+          <div>
+            <svg>
+              <use href="${icons}#icon-smile"></use>
+            </svg>
+          </div>
+          <p>${message}</p>
+        </div>
+        `;
+        this.#clear();
+        this.#parentElement.insertAdjacentHTML('afterbegin', markup);
+    } 
 
   addHandlerRender(handler) {
     ['hashchange', 'load'].forEach(ev => window.addEventListener(ev, handler));
@@ -130,4 +163,3 @@ class RecipeView {
 }
 
 export default new RecipeView();
-
